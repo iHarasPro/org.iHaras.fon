7eeb6cfcbcf494586768e1efd4d489c2b947765f
İbrahim Haras <git@iHaras.org>
2024-11-24 22:03:44 +0300

    Will fix

--- source/main.js
+++ source/main.js
@@ -1,5 +1,48 @@
-// There will be code lmao
+// Read an Object and build TransActions
+// transactions = {
+//   Date: Date
+//   Ticker: text
+//   Shares: integer
+//   Price: float
+//   }
+
+// Build two Objects
+// portfolio = {
+//   ticker1 = {
+//     summary = {
+//       shares: integer
+//       price: float
+//       value: float
+//     }
+//     details = {
+//       Date: Date
+//       Shares: integer
+//       price: float
+//     }
+//   }
+// }
 //
-// There will be code too xd
-//
-// Same old same old
+// realized = {
+//   ticker1 = {
+//     summary = {
+//       shares: integer
+//       price: float
+//       value: float
+//     }
+//     details = {
+//       Date: Date
+//       Shares: integer
+//       price: float
+//     }
+//   }
+// }
+
+// +----------+--------+-------+-------+-------+-------+
+// | Ticker   | Shares | Price | Value | Price | Value |
+// +----------+--------+-------+-------+-------+-------+
+// | GGK      |   1000 |  4.50 |  5.20 |  4500 |  5200 |
+// +----------+--------+-------+-------+-------+-------+
+// | YYYYDDMM |    400 |  4.50 | +0.70 |  1800 |  +280 |
+// | YYYYDDMM |    300 |  4.50 | +0.70 |  1350 |  +210 |
+// | YYYYDDMM |    300 |  4.50 | +0.70 |  1350 |  +210 |
+// +----------+--------+-------+-------+-------+-------+
